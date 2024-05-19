7ac51f5cd64d1fd4d25fcbc10a39a2f28a41337a
aadehamid <aadehamid@gmail.com>
2024-05-19 07:42:41 +0000

    Added the ability to install library locally with pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple local package',
+    author='Hamid Adesokan',
+    author_email='aadehamid@gmail.com',
+)
